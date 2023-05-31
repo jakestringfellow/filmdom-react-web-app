bc8e426d48029e4e8ad6ecf81b9290726880c5b1
Jake Stringfellow <stringfellow.j@northeastern.edu>
2023-05-31 11:39:49 -0400

    Updated output

--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -1,8 +1,19 @@
 import JavaScript from "./java-script";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoItem from "./todo/todo-item";
+import TodoList from "./todo/todo-list";
+
 function Assignment3() {
     return(
         <div>
             <h1>Assignment 3</h1>
+            <TodoList/>
+            <TodoItem/>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
             <JavaScript/>
         </div>
     );
