a57093e16519dcdb4fbc1246c8f644f56f8378f9
Jake Stringfellow <stringfellow.j@northeastern.edu>
2023-06-07 17:35:06 -0400

    Whats happening implementation bug fixed

--- src/tuiter/home-screen/index.js
+++ src/tuiter/home-screen/index.js
@@ -3,11 +3,12 @@ import TuitList from "../tuits/tuit-list";
 import { AiOutlineSearch } from "react-icons/ai";
 import { GoGear } from "react-icons/go";
 import "./index.css";
+import WhatsHappening from "../whats-happening";
 function HomeScreen() {
  return(
    <>
      <h2>Home</h2>
-     
+     <WhatsHappening/>
      <TuitList/>
    </>
  );
---
+++ src/tuiter/whats-happening.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import { AiOutlinePicture, AiOutlineGif } from "react-icons/ai";
+import { HiOutlineLocationMarker} from "react-icons/hi";
+import { MdFormatListBulleted } from "react-icons/md";
+import { BsEmojiSmile } from "react-icons/bs";
+import { TbCalendarStats } from "react-icons/tb";
+import { BiBold, BiItalic } from "react-icons/bi";
+
+const WhatsHappening = () => {
+ let [whatsHappening, setWhatsHappening] = useState('');
+ const tuitClickHandler = () => {
+   console.log(whatsHappening);
+ }
+ return (
+   <div className="row">
+     <div className="col-auto">
+       <img src="/images/nasa.png" width={60}/>
+     </div>
+     <div className="col-10">
+       <textarea value={whatsHappening} placeholder="What's happening?"
+               className="form-control border-0"
+               onChange={(event) => setWhatsHappening(event.target.value)}>
+       </textarea>
+       <div>
+         <button className="rounded-pill btn btn-primary float-end mt-2 ps-3 pe-3 fw-bold"
+                 onClick={tuitClickHandler}>
+           Tuit
+         </button>
+         <div className="text-primary fs-2">
+           <AiOutlinePicture className="me-3"/>
+           <AiOutlineGif className="me-3"/>
+           <MdFormatListBulleted className="me-3"/>
+           <BsEmojiSmile className="me-3"/>
+           <TbCalendarStats className="me-3"/>
+           <HiOutlineLocationMarker className="me-3"/>
+           <BiBold className="me-3"/>
+           <BiItalic className="me-3"/>
+         </div>
+       </div>
+     </div>
+     <div className="col-12"><hr/></div>
+   </div>
+ );
+}
+export default WhatsHappening;
