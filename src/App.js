a67e6281b32b57ec7f4ac9053dd20eb3b46ce3c7
Jake Stringfellow <stringfellow.j@northeastern.edu>
2023-06-06 22:03:12 -0400

    Switching from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Labs from "./labs";
 import HelloWorld from './labs/a3/hello-world';
 import Tuiter from './tuiter';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import {Routes, Route} from "react-router";
 import { Navigate } from "react-router";
 
